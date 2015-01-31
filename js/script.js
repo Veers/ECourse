c8b171d013618ae88052f9b50c22f5cfedc0ba1c
Veers <a.konviser@gmail.com>
2015-02-01 01:17:54 +0300

    refactor styles, add js for pdf, move pdf to other folder

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 window.onload = function ()
 {
     //var myPDF = new PDFObject({ url: "sample.pdf" }).embed();
-    var variablename = new PDFObject({ url: "1.pdf" }).embed("view");
+    var variablename = new PDFObject({ url: "../docs/1.pdf" }).embed("pdf_view");
 
+    var pdf_view = $('#pdf_view');
+    var parentPdf = $('#pdf_view').parent();
+    pdf_view.width(parentPdf.width());
+    var height = $('#sidebar-wrapper').height();
+    pdf_view.height(height);
+
+    alert('test');
 };
