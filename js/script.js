6c7f1a58b108e5d838a30530c750c4c5c557c28f
den4ik615 <den4ik615@rambler.ru>
2015-02-01 23:09:43 +0300

    delete string

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ window.onload = function ()
     //var myPDF = new PDFObject({ url: "sample.pdf" }).embed();
     var variablename = new PDFObject({ url: "../docs/1.pdf" }).embed("pdf_view");
 
-    var pdf_view = $('#pdf_view');
-    var parentPdf = $('#pdf_view').parent();
-    pdf_view.width(parentPdf.width());
-    var height = $('#sidebar-wrapper').height();
-    pdf_view.height(height);
+    //var pdf_view = $('#pdf_view');
+    //var parentPdf = $('#pdf_view').parent();
+    //pdf_view.width(parentPdf.width());
+    //var height = $('#sidebar-wrapper').height();
+    //pdf_view.height(height);
 
-    alert('test');
+    //alert('test');
 };
